3535fd8e3f34fa10963f2c2d8ffecc6685ff7fd0
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-01-30 19:16:20 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Week 2 Lab:" << endl;
+    int bob;
+    int john;
+
+    bob = 42;
+    john = 32;
+    int jimbob = bob + john;
+    short rachel = 10;
+    short luke = 7;
+    float pie = 3.14;
+    char middle = 'M';
+
+    cout << "Bob and John together are " << jimbob << " years old" << endl;
+
+    cout << middle << " is the middle initial ";
+    cout << pie << " users that are " << luke;
+    cout << " years old.";
+
+    return 0;
+}
